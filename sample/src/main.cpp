8bdfe98298f2a85df0a6d051030eff7667df1dc4
Luis Chiang <lhc4@njit.edu>
2020-05-16 04:03:55 -0400

    vulkan optional

--- sample/src/main.cpp
+++ sample/src/main.cpp
@@ -2,21 +2,30 @@
 #include "baka_logger.h"
 #include "baka_input.h"
 #include "baka_graphics.h"
-#include <math.h>
-#include <iostream>
 
-baka::Input *baka_in;
-
-int BakaMain(int argc, char *argv[])
+int BakaApplication::Main(int argc, char *argv[])
 {
     baka::Graphics::Init("Baka Engine", 1280, 720, 0);
+
+    #ifdef VULKAN_AVAILABLE
+    bakalog("vulkan available");
+    vk_graphics = new baka::VulkanGraphics();
+    #else
+    bakaerr("vulkan not found");
+    #endif
+
     return 0;
 }
 
-void BakaUpdate(float deltaTime)
+void BakaApplication::Update(float deltaTime)
 {
     if( baka::Input::KeyPressed(SDL_SCANCODE_R) )
     {
         bakalog("you just pressed R");
     }
+}
+
+BakaApplication::~BakaApplication()
+{
+
 }
