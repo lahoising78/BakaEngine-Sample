07ea8a32b7c79e5d0bf1e1a64bf820fbb97195fb
Luis Chiang <lhc4@njit.edu>
2020-05-17 18:52:17 -0400

    enable validations by default

--- sample/src/main.cpp
+++ sample/src/main.cpp
@@ -2,15 +2,23 @@
 #include "baka_logger.h"
 #include "baka_input.h"
 #include "baka_graphics.h"
-#include "vk_sample.h"
 
 int BakaApplication::Main(int argc, char *argv[])
 {
-    vk_graphics = new baka::VulkanGraphics("Baka Engine");
+    bool validations = true;
+    for(int i = 1; i < argc; i++)
+    {
+        if( strcmp(argv[i], "-nv") == 0 || strcmp(argv[i], "--no-validations") == 0 )
+        {
+            validations = false;
+        }
+    }
+
+    vk_graphics = new baka::VulkanGraphics("Baka Engine", validations);
 
     uint32_t flags = 0;
     if(vk_graphics) flags |= baka::GraphicAPI::VULKAN;
-    
+
     baka::Graphics::Init("Baka Engine", 1280, 720, flags);
 
     return 0;
