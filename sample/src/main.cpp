02774e406b086388f7ec1e85bad07da3c4ede511
Luis Chiang <lhc4@njit.edu>
2020-06-11 19:11:45 -0400

    logger update

--- sample/src/main.cpp
+++ sample/src/main.cpp
@@ -18,19 +18,44 @@ public:
 
     void Update() override
     {
-        BAKA_ASSERT(input);
-        
-        if( input->MouseButtonJustPressed(BAKA_MOUSE_BUTTON_LEFT) )
-            bakalog("left click ;)");
+        if( input->KeyJustPressed(BAKA_KEYCODE_Q) )
+            baka_log_filter_output(BAKA_LOG_BIT_INFO, BAKA_LOG_ACTION_SET);
 
-        if( input->IsKeyPressed(BAKA_KEYCODE_R) )
-            bakalog("you are pressing r");
+        else if ( input->KeyJustPressed(BAKA_KEYCODE_W) )
+            baka_log_filter_output(BAKA_LOG_BIT_WARN, BAKA_LOG_ACTION_SET);
+
+        else if ( input->KeyJustPressed(BAKA_KEYCODE_E) )
+            baka_log_filter_output(BAKA_LOG_BIT_ERR, BAKA_LOG_ACTION_SET);
+        
+        else if( input->KeyJustPressed(BAKA_KEYCODE_A) )
+            baka_log_filter_output(BAKA_LOG_BIT_INFO, BAKA_LOG_ACTION_ON);
+
+        else if ( input->KeyJustPressed(BAKA_KEYCODE_S) )
+            baka_log_filter_output(BAKA_LOG_BIT_WARN, BAKA_LOG_ACTION_ON);
+
+        else if ( input->KeyJustPressed(BAKA_KEYCODE_D) )
+            baka_log_filter_output(BAKA_LOG_BIT_ERR, BAKA_LOG_ACTION_ON);
+        
+        else if( input->KeyJustPressed(BAKA_KEYCODE_Z) )
+            baka_log_filter_output(BAKA_LOG_BIT_INFO, BAKA_LOG_ACTION_OFF);
+
+        else if ( input->KeyJustPressed(BAKA_KEYCODE_X) )
+            baka_log_filter_output(BAKA_LOG_BIT_WARN, BAKA_LOG_ACTION_OFF);
+
+        else if ( input->KeyJustPressed(BAKA_KEYCODE_C) )
+            baka_log_filter_output(BAKA_LOG_BIT_ERR, BAKA_LOG_ACTION_OFF);
+
+        else if ( input->KeyJustPressed(BAKA_KEYCODE_M) )
+        {
+            bakalog("this is info message");
+            bakawarn("this is warn message");
+            bakaerr("this is error message");
+        }
     }
 };
 
 int main(int argc, char *argv[])
 {
-    bakalog("hola, como esta?");
     SampleApp app = SampleApp();
     app.Start();
     return 0;
