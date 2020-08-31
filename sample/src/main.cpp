0bd44b7a664008db4677e619134dbec646922193
Luis Chiang <lhc4@njit.edu>
2020-08-31 01:28:04 -0400

    update engine

--- sample/src/main.cpp
+++ sample/src/main.cpp
@@ -15,35 +15,30 @@ public:
     {
         g_time = &baka::Time::Get();
 
-        app_config.window_config.width = 1280;
-        app_config.window_config.height = 720;
+        app_config.window_config.width = 800;
+        app_config.window_config.height = 600;
         snprintf(app_config.window_config.name, BAKA_WINDOW_NAME_MAX_LENGTH, "Sample Application");
+    }
+
+    ~SampleApp()
+    {
+        bakalog("sample app destroyed");
+    }
+
+    void Start() override
+    {
+        
     }
 
     void Update() override
     {
-        // bakalog("time in int64 since start: %li", g_time->GetTime());
-
-        if( g_input->IsMouseButtonPressed(BAKA_MOUSE_BUTTON_RIGHT) )
-        {
-            bakalog("Mouse button right is actively pressed");
-        }
-
-        if( g_input->MouseButtonJustPressed(BAKA_MOUSE_BUTTON_LEFT) )
-        {
-            bakalog("just pressed");
-        }
-
-        if(g_input->MouseButtonJustReleased(BAKA_MOUSE_BUTTON_LEFT))
-        {
-            bakalog("button released");
-        }
+        
     }
 };
 
 int main(int argc, char *argv[])
 {
     SampleApp app = SampleApp();
-    app.Start();
+    app.Run();
     return 0;
 }
